0af33e7f30d0608dfa2dfe388f43a815dc0ff361
iamrohit017 <133238101+iamrohit017@users.noreply.github.com>
2024-01-04 22:51:35 +0530

    Update Find element occuring once when all other are present thrice.cpp

--- Find element occuring once when all other are present thrice.cpp
+++ Find element occuring once when all other are present thrice.cpp
@@ -2,6 +2,19 @@
 //4th of January GFG POTD
 //problem link:https://www.geeksforgeeks.org/problems/find-element-occuring-once-when-all-other-are-present-thrice/1
 
+// Google : important question
+ int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int ans = 0;
+            for (int j = 0; j < N; j++) {
+                if ((nums[j] & (1 << i)) != 0)
+                    ans++;
+            }
+            if (ans % 3 == 1)
+                res += (1 << i);
+        }
+        return res;
+
 
 //{ Driver Code Starts
 //Initial Template for C++
